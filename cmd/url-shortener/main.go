a43f369f777fe7b712a6dba0ca94dae57100aef1
Nurtore Orazbek <nurtoreorazbek@Nurtores-MacBook-Air.local>
2025-04-08 16:45:08 +0500

    added pretty logging,and sqlite logic

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"log/slog"
 	"miniUrl/internal/config"
+	mwLogger "miniUrl/internal/http-server/middleware/logger"
+	"miniUrl/internal/lib/logger/handlers/slogpretty"
+	"miniUrl/internal/lib/logger/sl"
+	"miniUrl/internal/storage/sqlite"
 	"os"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
 )
 
 const (
@@ -16,19 +22,39 @@ const (
 func main() {
 
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
+
 	log := setupLogger(cfg.Env)
 	log.Info("starting url shortener", slog.String("env", cfg.Env))
 	log.Debug("debug messages are enabled")
+	log.Error("error mesages are enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	// id, _ := storage.SaveURL("https://chatgpt.com/", "chat")
+	// log.Info("id", id)
+
+	str, _ := storage.GetURL("chat")
+
+	log.Info("link for chat", slog.String("url", str))
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+	_ = storage
 }
 
 func setupLogger(env string) *slog.Logger {
 	var log *slog.Logger
 	switch env {
 	case envLocal:
-		log = slog.New(
-			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
-		)
+		log = setupPrettySlog()
 	case envDev:
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
@@ -37,7 +63,22 @@ func setupLogger(env string) *slog.Logger {
 		log = slog.New(
 			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
 		)
+	default: // If env config is invalid, set prod settings by default due to security
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
 	}
 	return log
 
+}
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
